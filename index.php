f6d451b27446c04c3bf16d1880ac3adf601c4c56
Mahdi_80 <140277799+mahdi-80@users.noreply.github.com>
2024-12-03 13:51:23 +0330

    Add files via upload

--- index.php
+++ index.php
@@ -12,21 +12,27 @@
 
     <div class="container">
         <div class="products">
-            <article class="product" onclick="showDetails('کارت گرافیک XYZ', 'توضیحات: کارت گرافیک با قدرت بالا برای بازی و کارهای گرافیکی.', '10,000,000 تومان')">
-                <img src="https://via.placeholder.com/300x200" alt="کارت گرافیک">
-                <h2>کارت گرافیک XYZ</h2>
-                <p>قیمت: 10,000,000 تومان</p>
+            <article class="product" onclick="showDetails('کارت گرافیک انویدیا جی فورس RTX 4090', ' کارت گرافیک با قدرت بالا برای بازی و کارهای گرافیکی.', '200,000,000 تومان')">
+                <img src="rtx4090.jpg" alt="کارت گرافیک">
+                <h2>کارت گرافیک انویدیا جی فورس RTX 4090</h2>
+                <p>قیمت: 200,000,000 تومان</p>
             </article>
-            <article class="product" onclick="showDetails('پردازنده ABC', 'توضیحات: پردازنده قدرتمند برای کارهای سنگین و بازی.', '5,000,000 تومان')">
-                <img src="https://via.placeholder.com/300x200" alt="پردازنده">
-                <h2>پردازنده ABC</h2>
-                <p>قیمت: 5,000,000 تومان</p>
+            <article class="product" onclick="showDetails('پردازنده اینتل Core i9-14900K', ' پردازنده قدرتمند برای کارهای سنگین و بازی.', '32,000,000 تومان')">
+                <img src="i9-14900k.jpg" alt="پردازنده">
+                <h2>پردازنده اینتل Core i9-14900</h2>
+                <p>قیمت: 32,000,000 تومان</p>
             </article>
-            <article class="product" onclick="showDetails('رم 16 گیگابایت', 'توضیحات: رم با سرعت بالا برای کارایی بهتر سیستم.', '2,500,000 تومان')">
-                <img src="https://via.placeholder.com/300x200" alt="رم">
-                <h2>رم 16 گیگابایت</h2>
-                <p>قیمت: 2,500,000 تومان</p>
+            <article class="product" onclick="showDetails('RAM - 32GB', ' رم دسکتاپ DDR4 دو کاناله 3200 مگاهرتز CL16 جی اسکیل سری Ripjaws V ظرفیت 32 گیگابایت', '8,500,000 تومان')">
+                <img src="ram32gig.jpg" alt="رم">
+                <h2>RAM - 32GB</h2>
+                <p>قیمت: 8,500,000 تومان</p>
             </article>
+            <article class="product" onclick="showDetails('Motherboard',' مادربرد ایسوس ROG STRIX Z690-F GAMING WIFI DDR5ASUS ROG STRIX Z690-F GAMING WIFI DDR5 LGA 1700 Motherboard' , 'قیمت : 20.000.000 تومان')">
+                <img src="motherboard.jpg" alt="مادربرد">
+                <h2>Motherboard</h2>
+                <p>قیمت: 20.000.000 تومان</p>
+            </article>
+
             <!-- سایر محصولات -->
         </div>
 
